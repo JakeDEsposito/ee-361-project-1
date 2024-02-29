1d1d51cb28dd5d741242b7ec90741d2cea79757e
cVita66 <vcifone99@gmail.com>
2024-02-29 14:42:35 -0500

    Added the main driver and continued work on the keywords in context

--- main.py
+++ main.py
@@ -31,19 +31,13 @@
 		1)
 
 '''
-# 			Summary of Methods:
-#
-#
-#
 # Libraries
+from LiuHuiPI import *
+from PlayfairCipher import *
+from KeyWordContext import *
 
-
-# Constants
-
-
-# Variable Declarations
-
-
+# 			Methods:
+################################################
 # Header Rubrick
 '''
     Purpose: 
@@ -56,3 +50,26 @@
 
 '''
 
+# Main Program
+
+# Main menu loop
+while (True):
+    # Prompt for program functionality
+    print("\nWhich part of the project would be executed? (input # from bellow)")
+    print("1. Liu Hiu's PI algorithm")
+    print("2. The Playfair cipher")
+    print("3. Keyword in context")
+    print("4. Exit")
+    user_input = input("Selection: ")
+    # Interpret user input
+    if user_input == "1":
+        #problemOne()
+        print("Problem One...")
+    elif user_input == "2":
+        #problemTwo()
+        print("Problem One...")
+    elif user_input == "3":
+        problemThree()
+    else:
+        break
+    input("\nPress Enter to continue...")  # Pause before looping
