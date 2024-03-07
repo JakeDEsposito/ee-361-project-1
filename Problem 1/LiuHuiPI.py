f15aa426f226663a0c491edbd2ca0d5c597f78cc
JakeDEsposito <97902257+JakeDEsposito@users.noreply.github.com>
2024-03-07 14:20:56 -0500

    Fixed code

--- Problem 1/LiuHuiPI.py
+++ Problem 1/LiuHuiPI.py
@@ -1,19 +1,30 @@
-from math import sqrt, pow
+from math import sqrt, pow, pi
 
-# Incomplete
+# Creates an approximation of PI using the Liu Hui approximation method.
 def pi_LiuHui(epsilon):
     assert epsilon > 0, "Epsilon must be greater than 0!"
 
     r = 1
     M = r
     sides = 3
-    while M >= epsilon:
+    while True:
         m = sqrt(pow(M / 2, 2) + pow(r - sqrt(pow(r, 2) - pow(M, 2) / 4), 2))
-        M = m
         sides *= 2
-
+        if m * sides - M * sides / 2 < epsilon:
+            break
+        M = m
     return m * sides
 
-#assert pi_LiuHui(0.0001) == 3.1415838921483186
 
-print(pi_LiuHui(0.0001))
+assert pi_LiuHui(0.0001) == 3.1415838921483186
+
+prev = None
+for x in [1, 0.1, 0.01, 0.001, 0.0001, 0.00001]:
+    current = pi_LiuHui(x)
+    assert current <= pi, "Result of PI approximation is more than PI!"
+
+    if prev is None:
+        prev = current
+    else:
+        assert current - prev > 0, f"Result of PI approximation with epsilon {str(x)} is larger than previous approximation!"
+        prev = current
